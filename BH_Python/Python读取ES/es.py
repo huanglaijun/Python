8015e6d68b602a98f293e9f4f40e786ba777df58
huanglaijun <1558105333@qq.com>
2022-10-14 14:32:39 +0800

    v1

--- BH_Python/Python读取ES/es.py
+++ BH_Python/Python读取ES/es.py
@@ -3,7 +3,9 @@ from datetime import date, datetime
 from sqlite3 import converters
 from elasticsearch import Elasticsearch
 import pandas as pd
+from flask import Flask, render_template, request, redirect
 
+# 此程序功能：读取ES制定索引，并将索引数据保存至本地Excel文件
 
 # 依赖关系
 # Installing collected packages: urllib3, certifi, elastic-transport, elasticsearch
@@ -28,53 +30,5 @@ for hits in value['hits']['hits']:
 
 # 将数据读取为DataFrame保存至本地Excel中
 data = pd.DataFrame(all_data)
-# print(data)
+print(data)
 data.to_csv("C:/Users/1/Desktop/归档事件.csv", encoding='gbk')
-
-# pandas读取ES数据
-# Create a DataFrame object
-# from pandasticsearch import DataFrame
-# df = DataFrame.from_es(url='http://10.8.1.30:9200/',
-#                        index='ausdata_iso_eventdata_202210')
-
-# # Print the schema(mapping) of the index
-# df.print_schema()
-# company
-# |-- employee
-#   |-- name: {'index': 'not_analyzed', 'type': 'string'}
-#   |-- age: {'type': 'integer'}
-#   |-- gender: {'index': 'not_analyzed', 'type': 'string'}
-
-# Inspect the columns
-# df.columns
-# #['name', 'age', 'gender']
-
-# # Denote a column
-# df.name
-# # Column('name')
-# df['age']
-# # Column('age')
-
-# # Projection
-# df.filter(df.age < 25).select('name', 'age').collect()
-# # [Row(age=12,name='Alice'), Row(age=11,name='Bob'), Row(age=13,name='Leo')]
-
-# # Print the rows into console
-# df.filter(df.age < 25).select('name').show(3)
-# # +------+
-# # | name |
-# # +------+
-# # | Alice|
-# # | Bob  |
-# # | Leo  |
-# # +------+
-
-# # Convert to Pandas object for subsequent analysis
-# df[df.gender == 'male'].agg(df.age.avg).to_pandas()
-# #    avg(age)
-# # 0        12
-
-# # Translate the DataFrame to an ES query (dictionary)
-# df[df.gender == 'male'].agg(df.age.avg).to_dict()
-# # {'query': {'filtered': {'filter': {'term': {'gender': 'male'}}}}, 'aggregations': {'avg(birthYear)':
-# # {'avg': {'field': 'birthYear'}}}, 'size': 0}
